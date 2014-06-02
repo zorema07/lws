e4d261b43ec66c611fbdbc97cdf21ee82c806dba
John Dapzar <johndapzar@yahoo.com>
2014-06-02 10:05:49 +0530

    Migrate User table

--- app/database/migrations/2014_05_26_101619_create_users_table.php
+++ app/database/migrations/2014_05_26_101619_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration {
 			$table->string('username');
 			$table->string('email');
 			$table->string('password');
+			$table->string('remember_token');
 			$table->timestamps();
 		});
 	}
